6518bf6ccd2319b2ef78fc4458a21485905a4a67
Kshitij_Yadav <kshitijy112@gmail.com>
2026-03-22 13:08:03 +0530

    Enable CORS for frontend

--- backend/src/main/java/com/urlshortener/backend/controller/UrlShortenerController.java
+++ backend/src/main/java/com/urlshortener/backend/controller/UrlShortenerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://url-shortener-spring.netlify.app")
 @RestController
 public class UrlShortenerController {
 
